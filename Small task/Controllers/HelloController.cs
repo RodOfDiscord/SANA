d71ddf840f3322bf9fb0fa2b4ffd078d0a87978a
Rod_Of_Harmony <ipz222_pis@student.ztu.edu.ua>
2023-03-25 17:11:12 +0200

    Add project files.

---
+++ Small task/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Small_task.Controllers
+{
+    public class HelloController : Controller
+    {      
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WelcomeString(string name)
+        {
+            ViewData["Message"]  = $"Welcome, {name}!";
+            return View();
+           
+        }
+    }
+}
